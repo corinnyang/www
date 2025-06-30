82d2751246b2b19c1682f59b44fad70df90e0963
corinnyang <corinnyang@gmail.com>
2025-06-30 16:48:38 +0900

    250630

--- app01/app.py
+++ app01/app.py
@@ -1,4 +1,17 @@
 print("안녕하세요")
 
-file = open('README.md', 'r')
-print(file.readline())
+file = open('app01\\text.txt', encoding='UTF-8', mode='w')
+file.write('안녕하세요\n')
+file.close()
+
+file = open('app01\\text.txt', encoding='UTF-8', mode='r')
+print(file.readlines())
+file.close()
+
+file = open('app01\\text.txt', encoding='UTF-8', mode='a')
+file.write('추가 내용\n')
+file.close()
+
+file = open('app01\\text.txt', encoding='UTF-8', mode='r')
+print(file.readlines())
+file.close()
---
+++ app01/app1.py
@@ -0,0 +1,7 @@
+f = open('app01\\question.csv', mode='r', encoding='UTF-8')
+arr = f.readlines()
+
+for row in arr:
+    row = row.replace("\n", "")
+    row = row.replace(",","")
+    print(row)
