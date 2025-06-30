59be4e88e13812ab8049d78d90f9808ef13265c8
corinnyang <corinnyang@gmail.com>
2025-06-30 17:32:20 +0900

    250630

---
+++ app01/app3.py
@@ -0,0 +1,16 @@
+def 출력변환():
+    f = open('app01\\question.csv', mode='r', encoding='UTF-8')
+    arr = f.readlines()
+    
+    arr2 = []
+    for row in arr:
+        row = row.replace("\n", "")
+        arr2.append( row.split(",") )
+    
+    for row in arr2:
+        행 = ""
+        for v in row:
+            행 += 'O' if v == 'O' else ' '
+        print(행)
+
+출력변환()
